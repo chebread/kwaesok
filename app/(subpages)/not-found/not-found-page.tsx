fb5716bf81d6af90997d5b8b117f33249fca082d
Cha Haneum <developer_haneum_11@icloud.com>
2025-03-23 19:04:47 +0900

    v1.0

--- app/(subpages)/not-found/not-found-page.tsx
+++ app/(subpages)/not-found/not-found-page.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link';
+import style from './not-found-page.module.scss';
+import Image from '@data/kwaesok-favicon.png';
+
 export default function NotFoundPage() {
-  return <div>404 Not Found</div>;
+  return (
+    <div
+      className={style.container}
+      style={{
+        backgroundImage: `url(${Image.src})`,
+      }}
+    >
+      <header className={style.header}>
+        <span>404 Not Found</span>
+      </header>
+      <main className={style.main}>
+        <Link href="/" className={style.link}>
+          <span>Return home</span>
+        </Link>
+      </main>
+    </div>
+  );
 }
